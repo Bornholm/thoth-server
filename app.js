983aa748ff58bc62d9ae0673ae0dff87ebe5e3f6
William PETIT <william.petit@ac-dijon.fr>
2013-06-10 17:26:04 +0200

    Base UI, routes angular.js

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var config = require('config');
 var http = require('http');
-var Sequelize = require('sequelize');
-var pass = require('./lib/passphrase');
-var auth = require('./lib/auth');
-var models = require('./lib/models')
+var mongoose = require('mongoose');
+var pass = require('./lib/util/passphrase');
 var app = express();
 
 // Assign HTTP middlewares
@@ -15,8 +13,7 @@ app.use(express.query());
 app.use(express.cookieParser());
 
 // Define HTTP routes
-var routes = require('./lib/routes');
-routes.bindTo(app);
+require('./lib/routes')(app);
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.errorHandler());
@@ -25,27 +22,12 @@ pass.askPassphrase(function(answer) {
 
 	if(answer.trim()) {
 
-		// Define Lynx server encryption passphrase
+		// Define Toth server encryption passphrase
 		app.set('passphrase', answer);
 
-		// Initialize Sequelize ORM
-		var sequelize = new Sequelize(
-			config.database.name, 
-			config.database.user, 
-			config.database.password, 
-			config.database.options
-		);
-
-		// Initialize models
-		models.initialize(sequelize);
-
-		// Create HTTP Server & start listening requests
-		var address = config.address === '*' ? '' : config.address;
-		var port = config.port || 80;
-
 		var server = http.createServer(app);
-		server.listen(port, address, function() {
-			console.log('Server listening on http://'+config.address+':'+port);
+		server.listen(config.web.port, config.web.address, function() {
+			console.log('Server listening on http://'+config.web.address+':'+config.web.port);
 		});
 
 	} else console.error("Passphrase shouldn't be empty. Aborting.");
---
+++ public/scripts/app.js
@@ -0,0 +1,42 @@
+(function(w) {
+
+	"use strict";
+
+	var angular = w.angular;
+
+	var Thoth = angular.module('Thoth', []);
+
+	Thoth.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+
+		$routeProvider.when('/search', {
+			templateUrl: 'templates/search.html'
+		});
+
+		//$routeProvider.otherwise({redirectTo: '/bookmarks'});
+
+		$routeProvider.html5Mode = false;
+
+	}]);
+
+	Thoth.controller('NavCtrl', ['$scope', '$location', function($scope, $location) {
+
+		$scope.navState = {
+			bookmarks: false,
+			'new-entry': false,
+			search: false,
+			admin: false,
+			profile: false
+		}
+
+		$scope.$location = $location
+
+		$scope.$watch('$location.path()', function() {
+			var path = $location.path();
+			Object.keys($scope.navState).forEach(function(key) {
+				$scope.navState[key] = (path === '/'+key);
+			});
+		});
+
+	}]);
+
+}(window))
